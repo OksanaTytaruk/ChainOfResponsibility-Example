ab52e63b06af6df60b34baf82d5e59223a47c10d
OksanaTytaruk <cassieroyster@gmail.com>
2024-10-07 01:02:40 +0300

    Create ChainOfResponsibilityExample.cs

---
+++ ChainOfResponsibilityExample.cs
@@ -0,0 +1,82 @@
+using System;
+
+// Базовий клас обробника
+abstract class Handler
+{
+    protected Handler _nextHandler;
+
+    public void SetNext(Handler nextHandler)
+    {
+        _nextHandler = nextHandler;
+    }
+
+    public abstract void HandleRequest(string request);
+}
+
+// Конкретний обробник A
+class ConcreteHandlerA : Handler
+{
+    public override void HandleRequest(string request)
+    {
+        if (request == "A")
+        {
+            Console.WriteLine("Обробник A обробив запит.");
+        }
+        else if (_nextHandler != null)
+        {
+            _nextHandler.HandleRequest(request);
+        }
+    }
+}
+
+// Конкретний обробник B
+class ConcreteHandlerB : Handler
+{
+    public override void HandleRequest(string request)
+    {
+        if (request == "B")
+        {
+            Console.WriteLine("Обробник B обробив запит.");
+        }
+        else if (_nextHandler != null)
+        {
+            _nextHandler.HandleRequest(request);
+        }
+    }
+}
+
+// Конкретний обробник C
+class ConcreteHandlerC : Handler
+{
+    public override void HandleRequest(string request)
+    {
+        if (request == "C")
+        {
+            Console.WriteLine("Обробник C обробив запит.");
+        }
+        else if (_nextHandler != null)
+        {
+            _nextHandler.HandleRequest(request);
+        }
+    }
+}
+
+// Клієнтський код
+class Program
+{
+    static void Main(string[] args)
+    {
+        var handlerA = new ConcreteHandlerA();
+        var handlerB = new ConcreteHandlerB();
+        var handlerC = new ConcreteHandlerC();
+
+        handlerA.SetNext(handlerB);
+        handlerB.SetNext(handlerC);
+
+        // Запити
+        handlerA.HandleRequest("A");
+        handlerA.HandleRequest("B");
+        handlerA.HandleRequest("C");
+        handlerA.HandleRequest("D");
+    }
+}
